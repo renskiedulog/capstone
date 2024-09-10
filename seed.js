d89cfde1eb61a0e45f970ab8ea145208272cc502
renskiedulog <22104607@usc.edu.ph>
2024-09-10 12:21:44 +0800

    Fix: Seeding Problems on Typescript

---
+++ seed.js
@@ -0,0 +1,110 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { models, Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    fullName: {
+      type: String,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    email: String,
+    address: String,
+    image: String,
+    contact: String,
+    birthdate: Date,
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "inactive",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || mongoose.model("User", userSchema);
+
+async function connectMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+async function disconnectDB() {
+  try {
+    await mongoose.connection.close();
+    console.log("Disconnected from MongoDB");
+  } catch (error) {
+    console.error("Error disconnecting from MongoDB:", error);
+  }
+}
+
+async function seedAdmin() {
+  try {
+    await connectMongoDB();
+
+    const adminUsername = "admin";
+    const adminPassword = "admin123";
+
+    const existingAdmin = await User.findOne({ username: adminUsername });
+
+    if (existingAdmin) {
+      console.log("Admin account already exists.");
+    } else {
+      const hashedPassword = await bcrypt.hash(adminPassword, 10);
+      await User.create({
+        username: adminUsername,
+        password: hashedPassword,
+        fullName: "Admin User",
+        firstName: "Admin",
+        lastName: "User",
+        email: "admin@example.com",
+        address: "ADMIN",
+        contact: "ADMIN",
+        birthdate: new Date("1970-01-01"),
+        image: null,
+        isAdmin: true,
+      });
+
+      console.log("Admin account created successfully.");
+    }
+
+    await disconnectDB();
+  } catch (error) {
+    console.error("Error seeding admin account:", error);
+    await disconnectDB();
+  }
+}
+
+seedAdmin();
