d83ca0168fceed0603e649ee6c8b1ea30ce6bf89
renskiedulog <22104607@usc.edu.ph>
2024-10-11 12:09:22 +0800

    Added: Auth Protection On New Pages

--- src/app/(public)/activity/page.tsx
+++ src/app/(public)/activity/page.tsx
@@ -1,6 +1,11 @@
+import { checkSession } from "@/components/utils/Authenticator";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  let session = await checkSession(); //! 1. Validate Session
+  if (!session) return redirect("/login"); //! 2. Avoid Any Unauthenticated Access
+
   return <div>page</div>;
 };
 
